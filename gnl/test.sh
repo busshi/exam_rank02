1518d20f73865a6f414bda2aa9dcb61b72a3b77d
aldubar <aldubar@student.42.fr>
2021-07-18 17:02:33 +0200

    update gnl tester

--- gnl/test.sh
+++ gnl/test.sh
@@ -7,7 +7,7 @@ clang -Wall -Werror -Wextra get_next_line.c main.c -o get_next_line
 ./get_next_line < get_next_line.c > gnl
 cat -e gnl > gnl_output
 cat -e < get_next_line.c > cat_output
-check=$(diff -y --suppress-common-line gnl_output cat_output > diff)
+diff -y --suppress-common-line gnl_output cat_output > diff
 res=$(cat diff | wc -l)
 ko=0
 [[ $res -ne 0 ]] && { ko=$(( $ko + 1 )); echo "GNL:KO"; echo -e "DIFF\tGNL vs cat:\n\n$(cat diff)"; } || { echo "GNL: OK"; rm -rf gnl gnl_output cat_output get_next_line diff; }
