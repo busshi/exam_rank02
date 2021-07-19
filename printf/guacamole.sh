9b469b9be0aa2b603040e9a1becfbaa8a7444639
aldubar <aldubar@student.42.fr>
2021-07-19 12:57:07 +0200

    fix coquille guacamole.sh

--- printf/guacamole.sh
+++ printf/guacamole.sh
@@ -2,6 +2,6 @@
 
 gcc -c ft_printf.c && ar rc libftprintf.a ft_printf.o
 
-git clone https://github.com/gavinfielder/pft.git pft_exam && echo "pft_exam/" >> .gitignore && cd pft_exam && rm unit_tests.c && rm options-config.ini && git clone https://github.com/cclaude42/PFT_EXAM.git temp && cp temp/unit_tests.c . && cp temp/options-config.ini . && rm -rf temp && make && ./test.sh
+git clone https://github.com/gavinfielder/pft.git pft_exam && echo "pft_exam/" >> .gitignore && cd pft_exam && rm unit_tests.c && rm options-config.ini && git clone https://github.com/cclaude42/PFT_EXAM.git temp && cp temp/unit_tests.c . && cp temp/options-config.ini . && rm -rf temp && make && ./test
 
 
